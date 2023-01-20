1a11ef6a602a285e8105d7dec3e2657b2bff4e2c
kacper.ryske <kacper.ryske@outlook.com>
2023-01-20 14:38:12 +0000

    20/01/2023
    Script does:
    -Automatically filling out the Plan Cost Grade Rates per band based on the Excel table

--- main.py
+++ main.py
@@ -52,6 +52,80 @@ with open('221021 Cost and Grade Rates.xlsx', 'rb') as f:
     file.decrypt(decrypted)
 
 df = pd.read_excel(decrypted, "Grade rates for Rapport Entry")
+dic = df.to_dict()
+keys = list(dic)
+
+k = 1
+
+key = keys[k]
 del (decrypted)
 
-print(df)
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
+class item:
+    def __init__(self, key, input, xpath):
+        self.key = key
+        self.input = input
+        self.xpath = xpath
+
+    def dropdown(self):
+        dropdown = driver.find_element(By.XPATH, self.xpath)
+        dd = Select(dropdown)
+        if self.input != " ":
+            dd.select_by_visible_text(self.input)
+        else:
+            pass
+
+    def textfill(self):
+        textfield = driver.find_element(By.XPATH, self.xpath)
+        if self.input != " ":
+            textfield.clear()
+            textfield.send_keys(self.input)
+        pass
+
+class field:
+
+    def __init__(self, xpath):
+        self.xpath = xpath
+    def select(self):
+        select = driver.find_element(By.XPATH, self.xpath)
+        select.click()
+
+
+WebDriverWait(driver, 3).until(EC.frame_to_be_available_and_switch_to_it("iframe1"))
+
+setting = field("//div[@title='Projects']")
+setting.select()
+
+setting2 = field("//div[@title='Test']")
+setting2.select()
+
+driver.switch_to.frame("iframe1")
+
+
+#select Branch - DCAL
+branch = item("DCAL", "DCAL" ,"//select[@id='BranchID']")
+branch.dropdown()
+
+driver.switch_to.frame("summaryframe")
+
+j = 0
+l = 0
+
+# Filling out the Plan Cost Grade Rates per band
+while k<11:
+    field1 = field(f"//div[@id='DefRates_{l}_GradeCostRate']")
+    field1.select()
+
+    item1 = item(key, truncate(dic[key][j], 2) ,"//input[@id='Grid_-1_txtGeneric']")
+    item1.textfill()
+
+    k = k + 1
+    key = keys[k]
+    l = l + 1
+
+# Filling out the Plan Charge Grade Rates per band TBC
+
+
